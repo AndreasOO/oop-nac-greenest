c56fe5cdfb54cd89f0bbb518b3f81593a95f258a
Andreas Ohlander <andreas.ohlander@gmail.com>
2024-09-27 08:41:57 +0200

    Merged package reordering branch

--- src/Greenest/Watering/Main.java
+++ src/Greenest/Watering/Main.java
@@ -1,11 +1,13 @@
 package Greenest.Watering;
 
-import Greenest.PlantCreation.PalmTree;
-import Greenest.PlantCreation.PlantFactory;
+import Greenest.PlantCreation.*;
 
 public class Main {
     public static void main(String[] args) {
         Owner owner = new Owner();
         PalmTree palmTree = PlantFactory.getPalmTree("test", 2.0);
+        Cactus cactus = PlantFactory.getCactus("test2", 3.0);
+
+
     }
 }
